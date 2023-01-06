ad63e49ab949ab0ffdc7af4ebd475e13c0363668
MaBad1 <abadiemathieu13@gmail.com>
2023-01-06 14:56:45 +0100

    rig + anim fleur bleue lvl 1

---
+++ Assets/Scripts/Anim.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Anim : MonoBehaviour
+{
+    private Animation anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animation>();
+        anim.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
